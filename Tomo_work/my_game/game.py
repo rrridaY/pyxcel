5c4d6b23c6edfae2e836e717f52b9ef871dba87e
gorispe <blaze_yu_zu@icloud.com>
2025-02-14 01:52:54 +0900

    ライブラリ拡張

--- Tomo_work/my_game/game.py
+++ Tomo_work/my_game/game.py
@@ -1,10 +1,11 @@
 import pyxel
 
-'''
-    固定値
-'''
+############################################################
+# 固定値
+############################################################
 WINDOW_WIDTH  = 160
 WINDOW_HEIGHT = 120
+
 
 ############################################################
 # Appクラス
@@ -13,7 +14,7 @@ class App:
     def __init__(self):
         pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title="Tomoゲーム")
         pyxel.mouse(True)
-        self.number = 0
+        pyxel.load("my_resource.pyxres")
         pyxel.run(self.update, self.draw)
 
 
@@ -24,13 +25,13 @@ class App:
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             if 20 <= pyxel.mouse_x <= 40 and 50 <= pyxel.mouse_y <= 70:
                 self.number -= 1
-            if 20 <= pyxel.mouse_
+            elif 100 <= pyxel.mouse_x <= 120 and 50 <= pyxel.mouse_y <= 70:
+                self.number += 1
+
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
-        pyxel.text(70, 60, f"{self.number}", pyxel.COLOR_YELLOW)
-        pyxel.text(30, 60, "-", pyxel.COLOR_WHITE)
-        pyxel.text(110, 60, "+", pyxel.COLOR_WHITE)
-
+        pyxel.blt(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK) # 表示したいx座標、y座標、イメージバンク、x座標　y座標、幅、高さ、透過として扱う色
+        
 
 App()
