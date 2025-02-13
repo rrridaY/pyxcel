1be2b5d677ee7253a08cd7bb8202240593364ebc
gorispe <blaze_yu_zu@icloud.com>
2025-02-14 02:42:52 +0900

    小見戸

--- Tomo_work/my_game/Object.py
+++ Tomo_work/my_game/Object.py
@@ -1,8 +1,9 @@
 #############################################
 # import
 #############################################
-import Vector
+from Vector import Vector2
 import Texture
+import pyxel
 
 #############################################
 # Objectクラス
@@ -12,5 +13,42 @@ import Texture
 class Object:
 
     def __init__(self, _x, _y, texture):
-        self.Pos = Vector.Vector2(_x, _y)
+        self.Pos = Vector2(_x, _y)
         self.Tex = texture
+    
+    # 更新
+    def Update():
+        pass
+
+    # 描画
+    def Render():
+        pass
+
+###############################################
+# 固定値
+###############################################
+PLAYER_MOVE_SPEED = 2
+PLAYER_JUMP_POWER = 5
+
+
+###############################################
+# Playerクラス
+###############################################
+class Player(Object):
+
+    # 入力処理
+    def Input(self):
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.Pos.x -= PLAYER_MOVE_SPEED
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.Pos.x += PLAYER_MOVE_SPEED
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.Pos.y += PLAYER_JUMP_POWER
+
+    def Update(self):
+        
+        # 入力処理
+        self.Input()
+
+    def Render(self):
+        pyxel.blt(self.Pos.x, self.Pos.y, self.Tex.img, self.Tex.Pos.x, self.Tex.Pos.y, self.Tex.width, self.Tex.height, self.Tex.colKey)
--- Tomo_work/my_game/game.py
+++ Tomo_work/my_game/game.py
@@ -1,4 +1,8 @@
 import pyxel
+import Object
+import Texture
+from Vector import Vector2
+from Object import Player 
 
 ############################################################
 # 固定値
@@ -15,23 +19,26 @@ class App:
         pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title="Tomoゲーム")
         pyxel.mouse(True)
         pyxel.load("my_resource.pyxres")
+
+        player = Player(Vector2(30, 30), Texture(0, Vector2(24, 0), 15, 15, pyxel.COLOR_BLACK))
+
+
         pyxel.run(self.update, self.draw)
+
 
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
 
-        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            if 20 <= pyxel.mouse_x <= 40 and 50 <= pyxel.mouse_y <= 70:
-                self.number -= 1
-            elif 100 <= pyxel.mouse_x <= 120 and 50 <= pyxel.mouse_y <= 70:
-                self.number += 1
+        player.Update()
+
 
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
-        pyxel.blt(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK) # 表示したいx座標、y座標、イメージバンク、x座標　y座標、幅、高さ、透過として扱う色
-        
+        # pyxel.blt(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK) # 表示したいx座標、y座標、イメージバンク、x座標　y座標、幅、高さ、透過として扱う色
+
+        player.Render()
 
 App()
